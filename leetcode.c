b8d932a4d2e7158eb241927ea2afd6522c07bbe4
20岁码农想当导演 <45226880+942418430@users.noreply.github.com>
2018-12-06 13:08:53 +0800

    Create leetcode.c

---
+++ leetcode.c
@@ -0,0 +1,13 @@
+bool isPalindrome(int x) {
+    int y = 0;
+    if(x<0 || ((x%10 == 0)&&(x!=0)))
+    {
+        return false;
+    }
+    while(x>y)
+    {
+        y = y*10 + x%10;
+        x /= 10;
+    }
+    return ((x == y) || (x == y/10));
+}
