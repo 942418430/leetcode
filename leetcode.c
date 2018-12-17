c1e2c2de50ec86431b761dad5c690e7d34785f31
20岁码农想当导演 <45226880+942418430@users.noreply.github.com>
2018-12-17 17:50:10 +0800

    Create leetcode.c
    给定一个整数数组，判断是否存在重复元素。
    
    如果任何值在数组中出现至少两次，函数返回 true。如果数组中每个元素都不相同，则返回 false。

---
+++ leetcode.c
@@ -0,0 +1,14 @@
+bool containsDuplicate(int* nums, int numsSize) {
+     int i=0;
+    int j=0;
+    for(i=0; i<numsSize; i++)
+    {
+        for(j=i+1; j<numsSize; j++)
+        {
+            if(nums[i] == nums[j])
+                return true;
+        }
+    }
+    return false;
+}
+     
