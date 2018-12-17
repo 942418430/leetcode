c475254cb3d51e365219ba4208bee235b506276b
20岁码农想当导演 <45226880+942418430@users.noreply.github.com>
2018-12-17 17:15:05 +0800

    Create leetcode_191.c
    编写一个函数，输入是一个无符号整数，返回其二进制表达式中数字位数为 ‘1’ 的个数（也被称为汉明重量）。

---
+++ leetcode_191.c
@@ -0,0 +1,13 @@
+int hammingWeight(uint32_t n) {
+    int count = 0;
+    int i = 0;
+    for(i=0; i<32; i++)
+    {
+        if((n&1) == 1)
+        {
+            count++;
+        }
+        n >>= 1;
+    }
+    return count;
+}
